31f1acc5b43bff4b2f6b63e7efb8180778be0d11
Soufiane Aitnaceur <sofiansofianait@gmail.com>
2026-05-29 17:45:32 +0100

    adding the health for the alert ingesting service

--- services/alert-ingestion/src/app.module.ts
+++ services/alert-ingestion/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
-
+import { HealthController } from './health/health.controller';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 @Module({
-  imports: [HttpModule],
-  controllers: [AppController],
+  imports: [
+    HttpModule,
+    PrometheusModule.register({
+      defaultMetrics: {
+        enabled: true,
+      },
+    }),
+  ],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
