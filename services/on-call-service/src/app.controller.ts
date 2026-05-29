04ca7a58792981d5deff29071e1076bcc9dc4e59
Soufiane Aitnaceur <sofiansofianait@gmail.com>
2026-05-29 17:45:32 +0100

    finishing the version that give the engnner that handle the incident but still need improvement

--- services/on-call-service/src/app.controller.ts
+++ services/on-call-service/src/app.controller.ts
@@ -1,21 +1,15 @@
 import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api/v1/on-call')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('current') // The specific endpoint
-  async getCurrentOnCall(
-    // 👇 THIS IS THE MAGIC LINE 👇
-    @Query('service') serviceName: string,
-  ) {
-    // 1. Validate: Did they actually send the parameter?
+  async getCurrentOnCall(@Query('service') serviceName: string) {
     if (!serviceName) {
       throw new BadRequestException('You must provide a ?service= parameter');
     }
-
-    // 2. Pass it to your Modulo Math logic!
     return await this.appService.getOnCallEngineer(serviceName);
   }
 }
--- services/on-call-service/src/app.service.ts
+++ services/on-call-service/src/app.service.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
+export interface OnCallResponse {
+  assigneeId: string;
+  name: string;
+  email: string;
+}
 @Injectable()
 export class AppService {
-  getOnCallEngineer(): {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(private prisma: PrismaService) {}
+  async getOnCallEngineer(serviceName: string): Promise<OnCallResponse> {
     //here the logic to get the current engenner to solve this issue
-  };
+    const team = await this.prisma.team.findUnique({
+      where: { name: serviceName },
+      include: {
+        engineers: {
+          orderBy: { email: 'asc' },
+        },
+      },
+    });
+    if (!team || team.engineers.length === 0) {
+      this.logger.error(
+        `No team or engineers found for service: ${serviceName}`,
+      );
+      throw new NotFoundException(
+        `No on-call rotation found for ${serviceName}`,
+      );
+    }
+    const engineers = team.engineers;
+    const millisecondsInWeek = 1000 * 60 * 60 * 24 * 7;
+    const currentWeekNumber = Math.floor(Date.now() / millisecondsInWeek);
+    const onCallIndex = currentWeekNumber % engineers.length;
+    const onCallEngineer = engineers[onCallIndex];
+
+    this.logger.log(
+      `📞 Calculated On-Call for ${serviceName}: ${onCallEngineer.name}`,
+    );
+
+    // 3. TypeScript will throw an error if this return object doesn't match the interface perfectly
+    return {
+      assigneeId: onCallEngineer.id,
+      name: onCallEngineer.name,
+      email: onCallEngineer.email,
+    };
+  }
 }
