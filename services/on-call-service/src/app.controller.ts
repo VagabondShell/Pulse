132a6337a0502ce5142e8d846c41344430c0b566
Soufiane Aitnaceur <sofiansofianait@gmail.com>
2026-05-29 17:45:32 +0100

    adding the service field in the schema for the incident

--- services/on-call-service/src/app.controller.ts
+++ services/on-call-service/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('current') // The specific endpoint
+  async getCurrentOnCall(
+    // 👇 THIS IS THE MAGIC LINE 👇
+    @Query('service') serviceName: string,
+  ) {
+    // 1. Validate: Did they actually send the parameter?
+    if (!serviceName) {
+      throw new BadRequestException('You must provide a ?service= parameter');
+    }
+
+    // 2. Pass it to your Modulo Math logic!
+    return await this.appService.getOnCallEngineer(serviceName);
   }
 }
--- services/on-call-service/src/app.service.ts
+++ services/on-call-service/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  getOnCallEngineer(): {
+    //here the logic to get the current engenner to solve this issue
+  };
 }
